42cb4ddeb0898f0067fb08e861777aa552ca2eac
ksktada <ksk.tada@gmail.com>
2020-08-02 23:01:46 +0900

    initial commit

---
+++ slackbot_settings.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+# SlackのAPIを利用するためのトークン
+# Botの設定ページから「OAuth & Permissions」のページに遷移し、
+# 「Bot User OAuth Access Token」をコピーして貼り付ける
+API_TOKEN = "xxx"
+
+# このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
+DEFAULT_REPLY = "I dont't understand you."
+
+# プラグインスクリプトを置いてあるサブディレクトリ名のリスト
+PLUGINS = ['plugins']
