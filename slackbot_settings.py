486d0e615f501fe5bc26d05604558901a59fc6fb
ksktada <ksk.tada@gmail.com>
2020-08-03 23:53:43 +0900

    pluginsがないのに定義してたためエラーで動かず.消したら動いた

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -3,10 +3,10 @@
 # SlackのAPIを利用するためのトークン
 # Botの設定ページから「OAuth & Permissions」のページに遷移し、
 # 「Bot User OAuth Access Token」をコピーして貼り付ける
-API_TOKEN = "xxx"
+API_TOKEN = "xoxb-1270135281542-1276880540018-uxPMPcEMMNisPIIOxl47khQv"
 
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
 DEFAULT_REPLY = "I dont't understand you."
 
 # プラグインスクリプトを置いてあるサブディレクトリ名のリスト
-PLUGINS = ['plugins']
+
