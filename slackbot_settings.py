75977ce9c6f68e77882ba1378882c371bdfe41bc
ksktada <ksk.tada@gmail.com>
2020-08-03 23:54:39 +0900

    token消し

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -3,7 +3,7 @@
 # SlackのAPIを利用するためのトークン
 # Botの設定ページから「OAuth & Permissions」のページに遷移し、
 # 「Bot User OAuth Access Token」をコピーして貼り付ける
-API_TOKEN = "xoxb-1270135281542-1276880540018-uxPMPcEMMNisPIIOxl47khQv"
+API_TOKEN = "xxx"
 
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
 DEFAULT_REPLY = "I dont't understand you."
